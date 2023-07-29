03e63a5ac88b103489a38e8be79e627a033e8181
pierre-luzi <pierre.luzi@gmail.com>
2023-07-29 22:54:08 +0200

    Ajout d'une description

--- labo_numerique/labo_num/models.py
+++ labo_numerique/labo_num/models.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.db import models
-from froala_editor.fields import FroalaField
 from django.core.validators import MaxValueValidator, MinValueValidator
 from datetime import datetime
 import json
