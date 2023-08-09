58bc3a123a1fc22ab6dfab7b2680b3860832d994
pierre-luzi <pierre.luzi@gmail.com>
2023-08-09 12:21:16 +0200

    Modification des urls

--- labo_numerique/labo_num/urls.py
+++ labo_numerique/labo_num/urls.py
@@ -4,8 +4,8 @@ import labo_num.views
 urlpatterns = [
     path('', labo_num.views.liste_animations, name='home'),
     path('ressources/<str:classe>/', labo_num.views.liste_chapitres, name='ressources'),
-    path('ressources/<str:classe>/<str:matiere><int:numero>/', labo_num.views.contenu_chapitre, name='chapitre'),
-    path('ressources/<str:classe>/<str:matiere><int:numero>/fiche_revision/', labo_num.views.fiche_revision, name='fiche_revision'),
+    path('ressources/<str:classe>/<str:matiere>/<int:numero>/', labo_num.views.contenu_chapitre, name='chapitre'),
+    path('ressources/<str:classe>/<str:matiere>/<int:numero>/fiche_revision/', labo_num.views.fiche_revision, name='fiche_revision'),
     path('annales/', labo_num.views.annales, name='annales'),
     path('animations/<slug:slug>/', labo_num.views.animation, name='animation'),
     path('animations/', labo_num.views.liste_animations, name='liste_animations'),
